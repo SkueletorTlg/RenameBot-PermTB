f8029cf52d703622065e3840efa4c02216733b1a
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-25 18:20:44 -0400

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -44,7 +44,8 @@ def send_start(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.START_TEXT.format(update.from_user.first_name),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="👤 Soporte", url="https://t.me/DKzippO")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="👤 Soporte", url="https://t.me/DKzippO")]
+                                                [InlineKeyboardButton(text="📢 Canales", url="https://t.me/CanalesFamosos"]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
