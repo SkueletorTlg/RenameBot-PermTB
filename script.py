3fef810f4a8b7bf4c8faddb3beb7bd024e3de5ea
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-02-25 20:27:01 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 class script(object):
-    START_TEXT = """¡Hola, mi principal función es renombrar archivos!
+    START_TEXT = """¡👋 Hola, mi principal función es renombrar archivos!
     	    
 <b>¡Envíame cualquier archivo de Telegram y elige la opción que desees!</b>
+
 Escriba /help para más información."""
 
     RENAME_403_ERR = "Perdón, tú no tienes permitido renombrar este archivo"
@@ -9,8 +10,8 @@ Escriba /help para más información."""
     DOWNLOAD_START = "😌 Intentando descargar a mi base de datos, espere por favor..."
     UPLOAD_START = "<b>😀 La descarga ha terminado, estoy intentando subir el archivo a Telegram...</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Gracias por usarme 🤓</b>\n Por favor, califícame si me encuentras útil: https://t.me/tlgrmcbot?start=renamearchive_bot-review ❤️"
-    SAVED_THUMB = "<b>Miniatura personalizada guardada ✅ Esto es permanente hasta que escribas /delthumb</b>"
-    DEL_THUMB = "Miniatura borrada con éxito 🤦"
+    SAVED_THUMB = "<b>Miniatura personalizada guardada ✅\n Esto es permanente, a menos que escribas /delthumb</b>"
+    DEL_THUMB = "Miniatura borrada con éxito 🤦🏻‍♂️"
     NO_THUMB = "No se encontraron miniaturas personalizadas"
     SAVED_RECVD_DOC_FILE = "<b>Archivo descargado correctamente 😎</b>"
     CUSTOM_CAPTION_UL_FILE = "Renombrado con: @RenameArchive_bot ❤️"
